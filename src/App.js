23d3794ab05d32dd126732ee58289ef2205763d3
romanakhatun <romanakhatun2004@gmail.com>
2020-11-22 10:02:31 +0600

    commit update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+// import NoMatch from './Components/NoMatch';
+import Home from './Components/Pages/Home';
+import Classes from './Components/Pages/Classes';
+import ClassPostDetail from './Components/Posts/ClassPostDetail';
+import Pricing from './Components/Pages/Pricing';
+import GymMembership from './Components/Pages/GymMembership';
+
+import classesData from './PostData/classesData';
+import pricingPlanData from './PostData/pricingPlanData';
+
+export const WebsiteName = "Power X";
+export const WebsiteSlogan = "Gym Website";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const classDetail = classesData.filter(singleClass => "/class/" + singleClass.permalink === window.location.pathname);
 
+	return (
+		<Router>
+			<Header></Header>
+
+			<Switch>
+				{classesData.length !== 0 && <Route path="/our-classes">
+					<Classes></Classes>
+				</Route>}
+
+				{classesData.length !== 0 && <Route path="/class/*">
+					{classDetail.map(singleClass => <ClassPostDetail classesData={singleClass} key={singleClass.id}></ClassPostDetail>)}
+				</Route>}
+
+				{pricingPlanData.length !== 0 && <Route path="/pricing">
+					<Pricing></Pricing>
+				</Route>}
+
+				<Route path="/gym-membership">
+					<GymMembership></GymMembership>
+				</Route>
+
+				<Route path="/">
+					<Home></Home>
+				</Route>
+
+				{/* <Route path="/*">
+                    <NoMatch></NoMatch>
+                </Route> */}
+			</Switch>
+
+			<Footer></Footer>
+		</Router>
+	);
+}
 export default App;
